d011fb794ec2e606f39c60dcf9b94f5ad8e8c7c2
ArslanMD Official <numberkliye@gmail.com>
2025-09-18 07:53:10 -0700

    αяѕℓαηм∂ σƒƒι¢ιαℓ

---
+++ config.js
@@ -0,0 +1,68 @@
+const fs = require('fs');
+const path = require('path');
+const { getConfig } = require("./lib/configdb");
+
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+
+module.exports = {
+    // ===== BOT CORE SETTINGS =====
+    SESSION_ID: process.env.SESSION_ID || "",  // Your bot's session ID (keep it secure)
+    PREFIX: getConfig("PREFIX") || ".",  // Command prefix (e.g., "., / ! * - +")
+    CHATBOT: getConfig("CHATBOT") || "on", // on/off chat bot 
+    BOT_NAME: process.env.BOT_NAME || getConfig("BOT_NAME") || "𝐀𝐫𝐬𝐥𝐚𝐧_𝐌𝐃",  // Bot's display name
+    MODE: getConfig("MODE") || process.env.MODE || "public",        // Bot mode: public/private/group/inbox
+    REPO: process.env.REPO || "https://github.com/Arslan-MD/Arslan-XMD",  // Bot's GitHub repo
+    BAILEYS: process.env.BAILEYS || "@whiskeysockets/baileys",  // Bot's BAILEYS
+
+    // ===== OWNER & DEVELOPER SETTINGS =====
+    OWNER_NUMBER: process.env.OWNER_NUMBER || "923237045919",  // Owner's WhatsApp number
+    OWNER_NAME: process.env.OWNER_NAME || getConfig("OWNER_NAME") || "αяѕℓαηм∂ σƒƒι¢ιαℓ",           // Owner's name
+    DEV: process.env.DEV || "923237045919",                     // Developer's contact number
+    DEVELOPER_NUMBER: '923237045919@s.whatsapp.net',            // Developer's WhatsApp ID
+
+    // ===== AUTO-RESPONSE SETTINGS =====
+    AUTO_REPLY: process.env.AUTO_REPLY || "false",              // Enable/disable auto-reply
+    AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",// Reply to status updates?
+    AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*ꜱᴛᴀᴛᴜꜱ ᴠɪᴇᴡᴇᴅ ʙy ᴀʀꜱʟᴀɴ-xᴍᴅ 🤖*",  // Status reply message
+    READ_MESSAGE: process.env.READ_MESSAGE || "false",          // Mark messages as read automatically?
+    REJECT_MSG: process.env.REJECT_MSG || "*📞 ᴄαℓℓ ɴσт αℓℓσωє∂ ιɴ тнιѕ ɴᴜмвєʀ уσυ ∂σɴт нανє ᴘєʀмιѕѕισɴ 📵*",
+    // ===== REACTION & STICKER SETTINGS =====
+    AUTO_REACT: process.env.AUTO_REACT || "false",              // Auto-react to messages?
+    OWNER_REACT: process.env.OWNER_REACT || "false",              // Auto-react to messages?
+    CUSTOM_REACT: process.env.CUSTOM_REACT || "false",          // Use custom emoji reactions?
+    CUSTOM_REACT_EMOJIS: getConfig("CUSTOM_REACT_EMOJIS") || process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍",  // set custom reacts
+    STICKER_NAME: process.env.STICKER_NAME || "𝐐𝐀𝐃𝐄𝐄𝐑-𝐀𝐈",     // Sticker pack name
+    AUTO_STICKER: process.env.AUTO_STICKER || "false",          // Auto-send stickers?
+    // ===== MEDIA & AUTOMATION =====
+    AUTO_RECORDING: process.env.AUTO_RECORDING || "false",      // Auto-record voice notes?
+    AUTO_TYPING: process.env.AUTO_TYPING || "false",            // Show typing indicator?
+    MENTION_REPLY: process.env.MENTION_REPLY || "false",   // reply on mentioned message 
+    MENU_IMAGE_URL: getConfig("MENU_IMAGE_URL") || "https://files.catbox.moe/x5csyw.jpg",  // Bot's "alive" menu mention image
+
+    // ===== SECURITY & ANTI-FEATURES =====
+    ANTI_DELETE: process.env.ANTI_DELETE || "true", // true antidelete to recover deleted messages 
+    ANTI_CALL: process.env.ANTI_CALL || "false", // enble to reject calls automatically 
+    ANTI_BAD_WORD: process.env.ANTI_BAD_WORD || "false",    // Block bad words?
+    ANTI_LINK: process.env.ANTI_LINK || "true",    // Block links in groups
+    ANTI_VV: process.env.ANTI_VV || "true",   // Block view-once messages
+    DELETE_LINKS: process.env.DELETE_LINKS || "false",          // Auto-delete links?
+    ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "same", // inbox deleted messages (or 'same' to resend)
+    ANTI_BOT: process.env.ANTI_BOT || "true",
+    PM_BLOCKER: process.env.PM_BLOCKER || "true",
+
+    // ===== BOT BEHAVIOR & APPEARANCE =====
+    DESCRIPTION: process.env.DESCRIPTION || "*_©αяѕℓαηм∂ σƒƒι¢ιαℓ*",  // Bot description
+    PUBLIC_MODE: process.env.PUBLIC_MODE || "true",              // Allow public commands?
+    ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",        // Show bot as always online?
+    AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true", // React to status updates?
+    AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true", // VIEW to status updates?
+    AUTO_BIO: process.env.AUTO_BIO || "false", // ture to get auto bio 
+    WELCOME: process.env.WELCOME || "false", // true to get welcome in groups 
+    GOODBYE: process.env.GOODBYE || "false", // true to get goodbye in groups 
+    ADMIN_ACTION: process.env.ADMIN_ACTION || "false", // true if want see admin activity 
+};
+        
